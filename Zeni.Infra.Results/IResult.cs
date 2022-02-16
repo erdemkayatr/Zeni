808bc5f72750580721e180f5bd12346e2bcda5f0
Erdem Kaya <erdem.kaya4@saglik.gov.tr>
2022-02-16 15:03:50 +0300

    .

---
+++ Zeni.Infra.Results/IResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Zeni.Infra.Results
+{
+    public interface IResult
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+    }
+
+    public interface IResult<out T> : IResult
+    {
+        T ResultData { get; }
+    }
+}
